692008bfd74614089cf48731ac87e4a1da03cfa0
amarees <saleh.amareen@dominos.com>
2021-11-08 11:38:29 -0500

    Removed comment

--- src/main/java/bjim/ClientApplication.java
+++ src/main/java/bjim/ClientApplication.java
@@ -8,7 +8,6 @@ public class ClientApplication {
 
 	public static void main(String[] args) {
 
-		// run client
 		Client user;
 		user = new Client("127.0.0.1");
 		user.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
